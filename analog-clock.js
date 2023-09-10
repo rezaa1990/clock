f6206b52a1f8f30f25da7dc3f5ee1fdee9348822
Reza <rezaaaa.z.1991@gmail.com>
2023-09-10 15:26:51 +0330

    number of clock added to canvas

--- analog-clock.js
+++ analog-clock.js
@@ -4,20 +4,30 @@ const context = canvas.getContext('2d');
 canvas.style.border = "0px";
 
 
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
+const centerX = canvas.width / 2 ;
+const centerY = canvas.height / 2 ;
 
 
-const radius = (canvas.width / 2) - 5;
+const radius = (canvas.width / 2) - 10;
 
 
 context.beginPath();
-context.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+context.arc(centerX, centerY, radius-20, 0, 2 * Math.PI);
 context.lineWidth = 3;
 context.strokeStyle = 'black';
 context.stroke();
 context.fillStyle = "#9F91CC"
 context.fill()
 
+////////////////////////////////////////////////////////////////////////////////
 
+for (let i = 1; i <= 12; i++) {
+    const angle = (i - 3) * (Math.PI / 6);
+    const x = centerX + Math.cos(angle) * radius;
+    const y = centerY + Math.sin(angle) * radius;
 
+    context.fillStyle = 'black';
+    context.font = '20px Arial';
+    context.fillText(i.toString(), x, y);
+  }
+///////////////////////////////////////////////////////////////////////////
